a77de8d7309071762f904ba64220a87e6d771ba0
AsadNoor25 <113261958+AsadNoor25@users.noreply.github.com>
2022-09-12 15:46:37 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
-//CYBER-X  DARKALPHAXTEAM ◎◎◎ https://github.com/darkalphaxteam\\
-//══════════════THANKS FOR CHEEMS BOT MD (DGNEON)═ALL THE CREADITS GOES TO HIM═════════════\\
-// EXAM ඉවර උනාම ඉතුරු ටික දෙන්නම් 😓😓😓 GOOD LUCK ALL\\
+//Asad Noor  TEAM A2A Gaming ARMY ◎◎◎ https://www.instagram.com/asadnoor_25/\\
+//══════════════THANKS FOR Asad BOT MD (A2A Gaming ARMY)═ALL THE CREADITS GOES TO HIM═════════════\\
+// EXAM I'll give you the rest when I'm done 😓😓😓 GOOD LUCK ALL\\
 const fs = require('fs')
 const chalk = require('chalk')
 
@@ -17,7 +17,7 @@ global.APIKeys = {
 
 //AUTO FUNCTIONS ( 📵 DONT EDIT 📵 ) ITS FOR NEXT UPDATE
 
-global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = false //auto reading in gc (true to on, false to off)
 global.autobio = false  // auto bio system checking on Developing Leval
@@ -36,32 +36,33 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
+global.vcardowner = ['03183241272'] //ur owner number
+global.ownername = "Asad Noor" //ur owner name
 global.ytname = "YT: TGM Arcade" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.location = "United State, California, US" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
-global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.owner = ['03183241272']
+global.ownertag = '903183241272' //ur tag number
+global.botnumber = '+1 (256) 685-1582' //Add your Bot Number For Log
+global.botname = 'Mishal' //ur bot name
+global.ig = "https://www.instagram.com/asadnoor_25/" //your theme url which will be displayed on whatsapp
+global.myweb = "https://www.instagram.com/asadnoor_25/" //ur website to be displayed
+global.sc = 'https://www.instagram.com/asadnoor_25/' //script link
+global.linkz = 'https://www.instagram.com/asadnoor_25/'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
-global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
-global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
-global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
-
+global.packname = "Asad Noor" //ur sticker watermark packname
+global.author = "Asad Noor" //ur sticker watermark author
+global.footer = "® Asad Noor"
+global.aliveimg ="https://imgur.com/e2MHulD.png"
+global.allmenuimg ="https://imgur.com/e2MHulD.png"
+global.welcomeimg= "https://imgur.com/e2MHulD.png"
+global.gbyeimg = "https://imgur.com/e2MHulD.png"
+global.alivemsg = "
+oba satuva næti dē labā gænīmē pahasuma kramaya  ægayīmayi.. hæki sǣma viṭama tava kenekva ægayīmaṭa utsaha karanna. eya obē mānasika saṁvardhita bavē salakuṇaki..\nsaṁskaraṇaya kaḷa parivartanayaki...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
+Appreciation is the easiest way to get what you don't have.. Try to appreciate someone else whenever possible. It is a sign of your mental development."
 //BOT MEDIAS
 global.thum = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 global.log0 = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur logo pic
@@ -70,7 +71,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['03183241272'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
